60fb44626b55bd19d7fe100259ddd813ed34d4b2
chiu <chiucode@gmail.com>
2015-06-09 02:09:40 +0000

    sugoi

--- 02_baby_steps.js
+++ 02_baby_steps.js
@@ -1,9 +1,9 @@
 console.log(process.argv);
 
-var sum;
-for(var i = 2; i<process.argv.length, i++){
+var sum = 0;
+for(var i = 2; i<process.argv.length; i++){
 
-sum += process.argv[i];
+sum += Number(process.argv[i]);
 
 }
 
