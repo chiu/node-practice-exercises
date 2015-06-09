b3b08c33570a7cb92576156f60b9ca31db8087fd
chiu <vchiucode@gmail.com>
2015-06-08 20:23:28 -0700

    done 4

--- 04_my_first_async_io.js
+++ 04_my_first_async_io.js
@@ -1,24 +1,45 @@
 // console.log(process.argv[2]);
 
 var fs = require('fs');
-var buf = fs.readFileSync(process.argv[2]);
-var str = buf.toString();
-var newline_count = str.split(/\r\n|\r|\n/).length - 1;
+// var buf = fs.readFile(process.argv[2]);
+// var str = buf.toString();
+// var newline_count = str.split(/\r\n|\r|\n/).length - 1;
 
-console.log(newline_count);
+// console.log(newline_count);
 
 
 
-var fs = require('fs') // require is a special function provided by node
-var myNumber = undefined // we don't know what the number is yet since it is stored in a file
+// var fs = require('fs') // require is a special function provided by node
+// var myNumber = undefined // we don't know what the number is yet since it is stored in a file
 
-function addOne() {
-  fs.readFile('number.txt', function doneReading(err, fileContents) {
-    myNumber = parseInt(fileContents)
-    myNumber++
-  })
-}
+// function addOne() {
+//   fs.readFile('number.txt', function doneReading(err, fileContents) {
+//     myNumber = parseInt(fileContents)
+//     myNumber++
+//   })
+// }
 
-addOne()
+// addOne()
 
-console.log(myNumber) // logs out undefined -- this line gets run before readFile is done
+// console.log(myNumber) // logs out undefined -- this line gets run before readFile is done
+
+
+fs.readFile(process.argv[2], function read(err, data) {
+  if (err) {
+    throw err;
+  }
+  content = data;
+  content = content.toString();
+
+
+  var newline_count = content.split(/\r\n|\r|\n/).length - 1;
+  console.log(newline_count);
+    // Invoke the next step here however you like
+    // console.log(content);   // Put all of the code here (not the best solution)
+    // processFile();
+  });
+
+
+// function processFile() {
+//   console.log(content);
+// }      
