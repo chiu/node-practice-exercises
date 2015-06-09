f58cdf70e23fadc14c9e913ade088aa277f8e083
chiu <vchiucode@gmail.com>
2015-06-08 20:34:10 -0700

    good

--- 04_my_first_async_io.js
+++ 04_my_first_async_io.js
@@ -1,30 +1,12 @@
 // console.log(process.argv[2]);
 
 var fs = require('fs');
-// var buf = fs.readFile(process.argv[2]);
-// var str = buf.toString();
-// var newline_count = str.split(/\r\n|\r|\n/).length - 1;
-
-// console.log(newline_count);
+var file = process.argv[2];
 
 
 
-// var fs = require('fs') // require is a special function provided by node
-// var myNumber = undefined // we don't know what the number is yet since it is stored in a file
 
-// function addOne() {
-//   fs.readFile('number.txt', function doneReading(err, fileContents) {
-//     myNumber = parseInt(fileContents)
-//     myNumber++
-//   })
-// }
-
-// addOne()
-
-// console.log(myNumber) // logs out undefined -- this line gets run before readFile is done
-
-
-fs.readFile(process.argv[2], function read(err, data) {
+fs.readFile(file, function read(err, data) {
   if (err) {
     throw err;
   }
@@ -34,12 +16,6 @@ fs.readFile(process.argv[2], function read(err, data) {
 
   var newline_count = content.split(/\r\n|\r|\n/).length - 1;
   console.log(newline_count);
-    // Invoke the next step here however you like
-    // console.log(content);   // Put all of the code here (not the best solution)
-    // processFile();
+    
   });
 
-
-// function processFile() {
-//   console.log(content);
-// }      
